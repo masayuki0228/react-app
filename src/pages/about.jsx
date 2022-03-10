aa4d860859f2d77c76759ae94fb6dc0d11d47b30
masayuki0228 <hirosawa0228@icloud.com>
2022-03-10 23:32:11 +0900

    ✨ useMemoについて学びました。

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,7 @@ import { Footer } from "src/components/Footer";
 
 export default function About(props) {
   const {
-    count,
+    doubleCount,
     isShow,
     handleClick,
     handleDisplay,
@@ -21,7 +21,7 @@ export default function About(props) {
         <title>About Page</title>
       </Head>
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
