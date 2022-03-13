99939499280cdcbef7d1bcc557a0d6e6c8c365c8
masayuki0228 <hirosawa0228@icloud.com>
2022-03-13 13:16:58 +0900

    🎨 replace to swr.

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,81 +1,25 @@
 import React from "react";
-import { useEffect, useCallback, useReducer } from "react";
-
-const initialState = {
-  data: [],
-  loading: true,
-  error: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end":
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    case "error":
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-    default: {
-      throw new Error("no such action type!");
-    }
-  }
-};
+import { usePosts } from "src/hooks/usePosts";
 
 export const Posts = () => {
-  const [state, diapatch] = useReducer(reducer, initialState);
-
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データの取得に失敗しました。");
-      }
-      const json = await res.json();
-      diapatch({ type: "end", data: json });
-    } catch (error) {
-      diapatch({ type: "error", error });
-    }
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  if (state.loading) {
+  const { data, error, isLoading, isEnpty } = usePosts();
+  if (isLoading) {
     return <div>ローディング中</div>;
   }
 
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
-  if (state.data.length === 0) {
-    <div>データは空です</div>;
+  if (isEnpty) {
+    return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {state.data.map((post) => {
+      {data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
   );
 };
-
-// ifが通ると、それ以降は全て表示されなくなる。
-// なので、コンポーネントを作って親と分ける必要がある。
-
-/*
- ...prevStateは、前回のオブジェクトを展開している
- オブジェクトにおいては、後に書いた方が勝つ
- スプレッド演算子を使って書き換えたいところを上書き
- data: [],
- loading: true,
- error: null,
- */
---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,22 @@
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error("An error occurred while fetching the data.");
+  }
+  return res.json();
+};
+
+export const usePosts = () => {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  return {
+    data,
+    error,
+    isLoading: !error && !data,
+    isEnpty: data && data.length === 0,
+  };
+};
