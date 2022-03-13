e33d8071cf71b33431b16408f92ee97e99689c89
masayuki0228 <hirosawa0228@icloud.com>
2022-03-13 10:37:38 +0900

    ⚡️ useStateを一つにまとめる

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useState, useCallback, useEffect } from "react";
 
 export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState([true]);
-  const [error, setError] = useState(null);
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+    error: null,
+  });
 
   const getPosts = useCallback(async () => {
     try {
@@ -13,37 +15,59 @@ export const Posts = () => {
         throw new Error("エラーが発生したため、データの取得に失敗しました。");
       }
       const json = await res.json();
-      setPosts(json);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          data: json,
+          loading: false,
+        };
+      });
     } catch (error) {
-      setError(error);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          loading: false,
+          error,
+        };
+      });
     }
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-// ifが通ると、それ以降は全て表示されなくなる。
-// なので、コンポーネントを作って親と分ける必要がある。
+  console.log("foo");
 
-  if (loading) {
+  // ifが通ると、それ以降は全て表示されなくなる。
+  // なので、コンポーネントを作って親と分ける必要がある。
+
+  if (state.loading) {
     return <div>ローディング中</div>;
   }
 
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
 
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {posts.map((post) => {
+      {state.data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
   );
 };
+
+/*
+ ...prevStateは、前回のオブジェクトを展開している
+ オブジェクトにおいては、後に書いた方が勝つ
+ スプレッド演算子を使って書き換えたいところを上書き
+ data: [],
+ loading: true,
+ error: null,
+ */
