a4705414789ae227a194a0157dbfaf5ec209ed12
masayuki0228 <hirosawa0228@icloud.com>
2022-03-17 22:33:17 +0900

    ⚡️ コメント一覧のページをSG化

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -2,15 +2,34 @@ import React from "react";
 import Head from "next/head";
 import { Header } from "src/components/Header";
 import { Comments as CommentsComponent } from "src/components/Comments";
+import { SWRConfig } from "swr";
 
-const Comments = () => {
+export const getStaticProps = async () => {
+  const COMMENTS＿API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  console.log("sg");
+  const comments = await fetch(COMMENTS＿API_URL);
+  const commentsData = await comments.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENTS＿API_URL]: commentsData,
+      },
+    },
+  };
+};
+
+const Comments = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Comments Page</title>
       </Head>
-      <Header />
-      <CommentsComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <CommentsComponent />
+      </SWRConfig>
     </div>
   );
 };
