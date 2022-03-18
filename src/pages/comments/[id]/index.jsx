47c33fc4107ecf84926f8ba0c23c785a76b1466c
masayuki0228 <hirosawa0228@icloud.com>
2022-03-18 18:21:56 +0900

    ✨ SGとfallbackについて学びました。

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -4,25 +4,34 @@ import { Comment } from "src/components/Comment";
 import { SWRConfig } from "swr";
 
 export const getStaticPaths = async () => {
-  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const comments = await fetch(
+    "https://jsonplaceholder.typicode.com/comments?_limit=10"
+  );
   const commentsData = await comments.json();
   const paths = commentsData.map((comment) => ({
     params: { id: comment.id.toString() },
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps = async (ctx) => {
   const { id } = ctx.params;
   const COMMENT＿API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
-  const comments = await fetch(COMMENT＿API_URL);
-  const commentsData = await comments.json();
+  const comment = await fetch(COMMENT＿API_URL);
+
+  if (!comment.ok) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const commentData = await comment.json();
 
   return {
     props: {
       fallback: {
-        [COMMENT＿API_URL]: commentsData,
+        [COMMENT＿API_URL]: commentData,
       },
     },
   };
@@ -30,6 +39,7 @@ export const getStaticProps = async (ctx) => {
 
 const CommentsId = (props) => {
   const { fallback } = props;
+
   return (
     <div>
       <Header />
